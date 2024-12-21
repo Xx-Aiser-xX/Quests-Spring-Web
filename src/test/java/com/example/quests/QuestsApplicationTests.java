b9f4a7b0582f1cf2c9d3f6638ab0572cc59fceb2
Xx-Aiser-xX <stas.plot@mail.ru>
2024-12-22 01:21:37 +0300

    init

---
+++ src/test/java/com/example/quests/QuestsApplicationTests.java
@@ -0,0 +1,38 @@
+package com.example.quests;
+
+import com.example.quests.entitys.Booking;
+import com.example.quests.entitys.Person;
+import com.example.quests.repositories.BookingRepository;
+import com.example.quests.repositories.PersonRepository;
+import com.example.quests.repositories.UserRepository;
+import com.example.quests.services.BookingService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestsApplicationTests {
+//
+//    @Autowired
+//    private BookingRepository bookingRepository;
+//    @Autowired
+//    private PersonRepository personRepository;
+//    @Autowired
+//    private UserRepository userRepository;
+//
+////    @Test
+////    void contextLoads() {
+////        bookingRepository.getAll(Booking.class, false);
+////    }
+////
+//    @Test
+//    void test() {
+//        var person = personRepository.personAndUser("Aboba@mail.ru");
+//    }
+////
+////    @Test
+////    void test1() {
+////        var person = userRepository.getUsers(0,5);
+////    }
+
+}
